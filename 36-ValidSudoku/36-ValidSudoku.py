383e5672123b7f3e394e088c15d4e98522575c1b
Aarsh Joshi <aarshjozhi@gmail.com>
2026-08-09 00:37:08 +0530

    Create 36-ValidSudoku

---
+++ 36-ValidSudoku/36-ValidSudoku.py
@@ -0,0 +1,25 @@
+# Last updated: 9/8/2026, 12:37:08 am
+class Solution(object):
+    def isValidSudoku(self, board):
+        rows = [0] * 9
+        cols = [0] * 9
+        squares = [0] * 9
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+
+                val = int(board[r][c]) - 1
+                if (1 << val) & rows[r]:
+                    return False
+                if (1 << val) & cols[c]:
+                    return False
+                if (1 << val) & squares[(r // 3) * 3 + (c // 3)]:
+                    return False
+
+                rows[r] |= (1 << val)
+                cols[c] |= (1 << val)
+                squares[(r // 3) * 3 + (c // 3)] |= (1 << val)
+
+        return True
