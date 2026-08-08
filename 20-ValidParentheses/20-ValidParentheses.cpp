99fe41438ae96e3463afcaecee5c9b95ebfb2421
Aarsh Joshi <aarshjozhi@gmail.com>
2026-08-09 00:37:07 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.cpp
@@ -0,0 +1,32 @@
+// Last updated: 9/8/2026, 12:37:07 am
+class Solution {
+public:
+    bool isValid(std::string s) {
+        std::stack<char> st;
+
+        for (char c : s) {
+
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } 
+
+            else {
+
+                if (st.empty()) {
+                    return false;
+                }
+
+                char top = st.top();
+
+                if ((c == ')' && top == '(') || 
+                    (c == '}' && top == '{') || 
+                    (c == ']' && top == '[')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
