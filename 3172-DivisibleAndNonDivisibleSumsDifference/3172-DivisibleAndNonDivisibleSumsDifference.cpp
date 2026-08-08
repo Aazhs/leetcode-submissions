6d64fbdf80f3ba0312d943cc2ffd22f4f820247d
Aarsh Joshi <aarshjozhi@gmail.com>
2026-08-09 00:35:53 +0530

    Create 3172-DivisibleAndNonDivisibleSumsDifference

---
+++ 3172-DivisibleAndNonDivisibleSumsDifference/3172-DivisibleAndNonDivisibleSumsDifference.cpp
@@ -0,0 +1,13 @@
+// Last updated: 9/8/2026, 12:35:53 am
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int num1 = 0, num2 = 0;
+
+        for (int i = 1; i <= n; i++){
+            if(i % m == 0) num2 += i;
+            else num1 += i;
+        }
+        return num1 - num2;
+    }
+};
